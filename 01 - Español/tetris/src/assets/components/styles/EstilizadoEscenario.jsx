36dda0860cf5aa17d92d341a8500b1f75eb6f67f
Anibal Cañete <anibal_gustavo2010@hotmail.com>
2026-01-01 02:27:37 -0300

    Creación del Componente EstilizadoEscenario.jsx

---
+++ 01 - Español/tetris/src/assets/components/styles/EstilizadoEscenario.jsx
@@ -0,0 +1,14 @@
+
+// Instalar (styled-components) con NPM (npm install styled-components) - Install (styled-components) with NPM (npm install styled-components)
+import styled from "styled-components";
+
+export const EstilizadoEscenario = styled.div`
+    display: grid;
+    grid-template-rows: repeat(${props => props.height}, calc(25vw / ${props => props.width}));
+    grid-template-columns: repeat(${props => props.width}, 1fr);
+    grid-gap: 1px;
+    border: 2px solid #333;
+    width: 100%;
+    max-width: 25vw;
+    background: #111;
+`;
