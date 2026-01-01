fe0c57b621d97e698e064a698e0de76ca579e5ae
Anibal Cañete <anibal_gustavo2010@hotmail.com>
2026-01-01 02:26:20 -0300

    Creación del Componente EstilizadoDisplay.jsx

---
+++ 01 - Español/tetris/src/assets/components/styles/EstilizadoDisplay.jsx
@@ -0,0 +1,19 @@
+
+// Instalar (styled-components) con NPM (npm install styled-components) - Install (styled-components) with NPM (npm install styled-components)
+import styled from "styled-components";
+
+export const EstilizadoDisplay = styled.div`
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    margin: 0 0 20px 0;
+    padding: 20px;
+    border: 4px solid #333;
+    min-height: 30px;
+    width: 100%;
+    border-radius: 20px;
+    color: ${({ $juegoTerminado }) => ($juegoTerminado ? "red" : "#999")};
+    background: #000;
+    font-family: Pixel, Arial, Helvetica, sans-serif;
+    font-size: 0.8rem;
+`;
